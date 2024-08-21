ef5b179f519b246fd4df594662d5355060e30590
moonjisu <jjisu97@naver.com>
2024-08-21 21:52:18 +0900

    24.08.21

--- my-game/src/game.jsx
+++ my-game/src/game.jsx
@@ -1,14 +1,75 @@
-import React from "react";
-import Board from "./board";
+import React, { useState } from "react";
+import Board from "./Board";
 
 function Game() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current.squares);
+
+  function handleClick(i) {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[historyPoint.length - 1];
+    const squares = current.squares.slice();
+
+    if (winner || squares[i]) return;
+
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(historyPoint.concat([{ squares }]));
+    setStepNumber(historyPoint.length);
+    setXIsNext(!xIsNext);
+  }
+
+  function jumpTo(step) {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  }
+
+  const status = winner
+    ? "Winner: " + winner
+    : "Next player: " + (xIsNext ? "X" : "O");
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>
+          {history.map((_, move) => (
+            <li key={move}>
+              <button onClick={() => jumpTo(move)}>
+                {move ? "Go to move #" + move : "Go to game start"}
+              </button>
+            </li>
+          ))}
+        </ol>
       </div>
     </div>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Game;
